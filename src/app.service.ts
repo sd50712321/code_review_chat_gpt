e14fb6d433d819dd6eb09ddca6822f373d2ec069
SangMin <sd50712321@gmail.com>
2023-03-27 12:46:08 +0900

    add, modified 된 파일만

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHello2(): string {
+    const 한글 = 'kor';
+    for (let i = 0; i < 3; i++) {
+      console.log('i = ', i);
+    }
+    return 'Hello World!';
+  }
 }
