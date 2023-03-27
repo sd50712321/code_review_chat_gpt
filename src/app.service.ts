0306e5e90fe2ca85b7b9bd8d2b930995dc4a63da
SangMin <sd50712321@gmail.com>
2023-03-27 12:26:20 +0900

    프롬프트에 네이밍컨벤션 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getHello2(): string {
+    const 헬로네임 = 'hello';
+    for (let i = 0; i < 3; i++) {
+      console.log('i =', i);
+    }
+    return 'Hello World!';
+  }
 }
